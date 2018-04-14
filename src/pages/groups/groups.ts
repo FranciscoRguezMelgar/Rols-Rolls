7e6c7ae4afa7d5e6c8c4575bf7331ad6f90d0683
FranciscoRguezM <100317282@alumnos.uc3m.es>
2018-04-14 18:46:10 +0200

    prueba 3 btooth

--- src/pages/groups/groups.ts
+++ src/pages/groups/groups.ts
@@ -16,11 +16,13 @@ import { BtComp } from '../../logic/btComp';
 })
 export class GroupsPage {
 	public devices = [{name:"Don Pepito", address:"AA:AA:AA:AA:AA:AA"}];
+	public texto = "";
   constructor(public navCtrl: NavController, public navParams: NavParams, public btc:BtComp) {
   }
 
   buscar(){
   	this.btc.search(this.devices);
+  	this.texto = JSON.stringify(this.devices)
   }
   setVisible(){
   	this.btc.setVisible()
