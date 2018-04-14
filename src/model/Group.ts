279854785231820ed22400172929bdd4f70fae14
FranciscoRguezM <100317282@alumnos.uc3m.es>
2018-04-14 21:53:13 +0200

    persistencia 0.5

--- src/model/Group.ts
+++ src/model/Group.ts
@@ -2,9 +2,11 @@ import { User } from "./User";
 export class Group{
 	public members: User[];
 	public name:string;
+	
 	constructor(n:string, m:Array<User>){
 		this.members = m;
 		this.name = n;
+		
 	}
 	addMember(n: User){
 		this.members.push(n);
